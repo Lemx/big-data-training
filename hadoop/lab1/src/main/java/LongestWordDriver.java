b6d457ce24fc12afc38826ba107d37cfc8a68bc5
Anatolii Fetisov <anatoliy.fetisov@me.com>
2016-11-01 02:37:48 +0300

    lab1 multiple files support

--- hadoop/lab1/src/main/java/LongestWordDriver.java
+++ hadoop/lab1/src/main/java/LongestWordDriver.java
@@ -4,13 +4,13 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-import org.apache.hadoop.util.GenericOptionsParser;
 
 public class LongestWordDriver extends Configured implements Tool {
 
@@ -26,8 +26,8 @@ public class LongestWordDriver extends Configured implements Tool {
         Job job = Job.getInstance(conf);
 
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (otherArgs.length != 2) {
-            System.err.println("Please, specify exactly two parameters: <in-file> <out-dir>");
+        if (otherArgs.length < 2) {
+            System.err.println("Please, specify at least two parameters: <in-file>* <out-dir>");
             System.exit(2);
         }
 
@@ -44,8 +44,11 @@ public class LongestWordDriver extends Configured implements Tool {
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        for (Integer i = 0; i < otherArgs.length - 1; i++) {
+            MultipleInputs.addInputPath(job, new Path(otherArgs[i]), TextInputFormat.class, LongestWordMapper.class);
+        }
+
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[otherArgs.length - 1]));
 
         return job.waitForCompletion(true) ? 0: 1;
     }
